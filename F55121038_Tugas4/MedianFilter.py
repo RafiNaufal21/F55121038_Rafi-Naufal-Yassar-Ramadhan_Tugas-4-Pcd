184a5f39d1a700a07964a76c6d10579fc3880a5a
RafiNaufalYassarRamadhan <114968568+RafiNaufalYassarRamadhan@users.noreply.github.com>
2023-03-13 18:48:52 +0800

    Add files via upload

---
+++ F55121038_Tugas4/MedianFilter.py
@@ -0,0 +1,7 @@
+import cv2
+img = cv2.imread('lena.png')
+filtered = cv2.medianBlur(img, 3)
+cv2.imshow('Original Image', img)
+cv2.imshow('Filtered Image', filtered)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
